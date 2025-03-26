e8f87d21680444f05b711e9aa3982cb1b58e7765
irmnt <irumikikuchi@gmail.com>
2025-03-25 21:33:23 -0400

    update LoanDetails

--- Models/LoanDetails.cs
+++ Models/LoanDetails.cs
@@ -4,31 +4,10 @@ namespace CarLoanCalculator
 {
     public class LoanDetails
     {
-        public int Id { get; set; }
-        public decimal LoanAmount { get; set; }
-        public decimal InterestRate { get; set; }
-        public int LoanTerm { get; set; }
+        public class LoanViewModel
+        {
+            public string Amount { get; set; }
+            public string Term { get; set; }
+        }
     }
-    //public partial class YourPage : System.Web.UI.Page
-    //{
-    //    protected void btnConfirm_Click(object sender, EventArgs e)
-    //    {
-    //        // Get selected radio value
-    //        string selectedPlan = HttpContext.Request.Form["loanPlan"]; 
-    //        HttpContext.Current.Response.Write("Selected Plan: " + selectedPlan);
-
-    //        //string name = txtName.Text; // Get value from TextBox
-
-    //        //if (string.IsNullOrWhiteSpace(name))
-    //        //{
-    //        //    lblErrorMessage.Text = "Name is required!";
-    //        //    lblErrorMessage.Visible = true; // Show error label
-    //        //}
-    //        //else
-    //        //{
-    //        //    lblErrorMessage.Visible = false; // Hide error label
-    //        //    // Process input (e.g., save to database)
-    //        //}
-    //    }
-    //}
 }

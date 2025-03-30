7a4eed29dfb0d2a5266e6221698e8a9b22206778
irmnt <irumikikuchi@gmail.com>
2025-03-29 23:47:23 -0400

    updated LoanViewModel

--- Models/LoanViewModel.cs
+++ Models/LoanViewModel.cs
@@ -2,8 +2,20 @@
 {
     public class LoanViewModel
     {
-        public decimal AnnualInterestRate { get; set; }
-        public decimal MonthlyPayment { get; set; }
+        // Values of Plan A
         public char SelectedPlan { get; set; }
+        public double TotalPaymentAmountPlanA { get; set; }
+        public double VehiclePricePlanA { get; set; }
+        public double DownPaymentPlanA { get; set; }
+        public string InsuranceTypePlanA { get; set; }
+        public double OtherFeesPlanA { get; set; }
+        public decimal TaxRatePlanA { get; set; }
+        public string LoanTermPlanA { get; set; }
+        public string LoanStartDatePlanA { get; set; }
+        public double InterestRatePlanA { get; set; }
+        public double TotalInterestPaidPlanA { get; set; }
+        public double TotalLoanAmountPlanA { get; set; }
+        public double MonthlyPaymentPlanA { get; set; }
+
     }
 }
